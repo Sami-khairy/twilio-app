f9a18f5f115f965cbc20bf7c5d2021e310da09fe
Sami-Khairy <samikhairy2003@gmail.com>
2025-03-29 17:31:20 +0000

    init app

---
+++ src/main/java/ma/khairy/twilio_app/TwilioAppApplication.java
@@ -0,0 +1,13 @@
+package ma.khairy.twilio_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/khairy/twilio_app/TwilioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.khairy.twilio_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
